295b4828ed56b9d31f38284019f7aec9920dbdd3
Edson Caliman <edsonjunior@db1.com.br>
2021-03-09 11:07:31 -0300

    student worker console

---
+++ StudentWorkerConsole/StudentWorkerConsole/Domain/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StudentWorkerConsole.Domain
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created_Date { get; set; }
+    }
+}
---
+++ StudentWorkerConsole/StudentWorkerConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using StudentWorkerConsole.Domain;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace StudentWorkerConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "studentQueue",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var student = JsonSerializer.Deserialize<Student>(message);
+                Console.WriteLine($"Received {student.Id} | {student.Name} | {student.Created_Date}");
+            };
+            channel.BasicConsume(queue: "studentQueue",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
